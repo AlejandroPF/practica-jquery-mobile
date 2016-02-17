1ea3b5d250152f70c7e414cbe628b3a7302604cb
Alejandro Peña Florentín <alejandropenaflorentin@gmail.com>
2016-02-17 10:34:14 +0100

    Creada autenticación

---
+++ js/custom.plugins.js
@@ -0,0 +1,24 @@
+(function ($) {
+    var source = "http://localhost/DIW/jquery-mobile-app-api/";
+    /**
+     * Autentica al usuario y establece la cookie de inicio de sesión
+     * @param {string} user Nombre de usuario
+     * @param {string} password Contraseña
+     * @param {callback} successCallback Función que se ejecuta si el usuario se ha autenticado correctamente
+     */
+    $.authenticate = function (user, password, successCallback) {
+        var data = {
+            user: user,
+            password: password,
+        };
+        $.ajax({
+            url: source + "authenticate/",
+            method: "get",
+            data: data
+        }).success(function(response){
+            successCallback(response)
+        }).error(function(){
+            console.log("[ERROR] USER AUTH");
+        });
+    }
+}(jQuery));
