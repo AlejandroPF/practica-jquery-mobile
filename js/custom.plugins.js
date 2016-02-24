21a0c615a59c095d865a97409fb1f254612822a2
Alejandro Peña Florentín <alejandropenaflorentin@gmail.com>
2016-02-24 20:00:30 +0100

    Agregado modulo jquery-mobile-spinbox. Agregada parte de evaluación

--- js/custom.plugins.js
+++ js/custom.plugins.js
@@ -1,4 +1,5 @@
 (function ($) {
+    // Establece la ruta de la api de la aplicación
     $.source = "http://localhost/DIW/jquery-mobile-app-api/";
     /**
      * Autentica al usuario y establece la cookie de inicio de sesión
@@ -31,5 +32,44 @@
             text: "Cargando",
             textVisible: true
         });
-    }
+    },
+            $.smiles = function () {
+                $("em").append($("<i></i>").attr({
+                    class: "fa fa-smile-o fa-3x",
+                    "data-type": "smile"
+                }));
+                $("em").append($("<i></i>").attr({
+                    class: "fa fa-meh-o fa-3x",
+                    "data-type": "meh"
+                }));
+                $("em").append($("<i></i>").attr({
+                    class: "fa fa-frown-o fa-3x",
+                    "data-type": "sad"
+                }));
+                $("i").click(function () {
+                    var cli = $(this).attr("data-type");
+                    console.log(cli)
+                    switch (cli) {
+                        //verde
+                        case "smile":
+                            $(this).css("color", "lime") //lime
+                            $(this).next().css("color", "white");
+                            $(this).nextAll().eq(1).css("color", "white");
+                            break;
+                        case "meh":
+                            $(this).css("color", "orange") //orange
+                            $(this).prev().css("color", "white")
+                            $(this).next().css("color", "white")
+                            break;
+                            //rojo
+                        case "sad":
+                            $(this).css("color", "red");
+                            $(this).prevAll().eq(0).css("color", "white");
+                            console.log($(this).prevAll().eq(0).attr("data-type") + "meh");
+                            console.log($(this).prevAll().eq(1).attr("data-type") + "smile")
+                            $(this).prevAll().eq(1).css("color", "white");
+                            break;
+                    }
+                });
+            };
 }(jQuery));
